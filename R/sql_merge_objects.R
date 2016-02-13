4413fe418239211400e2ea880c2498952b7fca42
Swarchal <S.Warchal@sms.ed.ac.uk>
2016-02-13 14:39:42 +0000

    add functions for extracting metadata

--- R/sql_merge_objects.R
+++ R/sql_merge_objects.R
@@ -1,21 +1,59 @@
+#' Get metadata columns
+#'
+#' Returns metadata columns from dataframe, by grep'ing columns beginning
+#' with 'Metadata_'
+#'
+#' @param table dataframe or table within database
+#' @param value If \code{TRUE} will return the column names, if \code{FALSE}
+#'    will return the indicies
+#'
+#' @export
+
+metadata_cols <- function(table, value = TRUE){
+    grep("Metadata_", colnames(table), value)
+}
+
+
+
+
+#' Get featuredata columns
+#'
+#' Returns the feature data columns from a dataframe, feature data is classed
+#'  as anything that is not metadata
+#'
+#' @param table dataframe of table within database
+#' @param value If \code{TRUE} will return the column names, if \code{FALSE}
+#'   will return the indicies
+#'
+#' @export
+
+featuredata_cols <- function(table, value = TRUE){
+    setdiff(colnames(table), grep("Metadata_", colnames(x)))
+}
+
+
+
+
+
 #' Merge object data
-#' 
+#'
 #' Function to merge together multiple objects from a pipeline into a single
 #' table and prefix the column names with the object label.
-#' 
+#'
 #' @param db database connection
-#' @param metrics helper file detailing pipelines and objects
+#' @param metrics helper filepath to file detailing pipelines and objects
 #' @param sep separator to separate pipeline and object in the filename
 #'      only used if a metric file is not supplied
-#' 
+#'
+#' @import DBI
 #' @import RSQLite
 #' @import stringr
-#' 
+#'
 #' @export
 
 
 sql_merge_objects <- function(db, metrics = NULL, sep = "_"){
-    
+
     # check if `db` is a valid database connection,
     # otherwise initialise a connection to `db`
     if (is.character(db)){
@@ -23,34 +61,34 @@ sql_merge_objects <- function(db, metrics = NULL, sep = "_"){
         con <- dbConnect(SQLite(), db)
         if (dbIsValid(con)) message(" - Connected!")
     }
-    
+
     if (!dbIsValid(con)) stop("Failed to connect to database")
-    
+
     # all tables contained within db
     list_of_tables <- dbListTables(con)
-    
+
     # get tables labelled as _Image
     image_tables <- str_subset(list_of_tables, "Image")
-    
+
     if (is.null(metrics)){
         message(" - No metric file supplied: Attempting to separate pipeline and objects")
         split_list <- str_split(list_of_tables, sep)
-        pipelines <- unique(sapply(split_list, function(x) x[1])) # first subelement 
+        pipelines <- unique(sapply(split_list, function(x) x[1])) # first subelement
         objects <- unique(sapply(split_list, function(x) x[2])) # second subelement
-        
+
     } else if (!isnull(metrics)){
         message(" - Reading pipeline and object info from ", metrics)
         metrics <- read.csv(metrics)
         pipelines <- unique(metrics$pipelines)
         objects <- unique(metrics$objects)
     }
-    
+
     #TODO identify featuredata and metadata
     #TODO prefix featuredata with object name
     #TODO merge together tables from the same pipeline by ImageNumber
     #TODO remove duplicate metadata columns
     #TODO give new name to merged tables
-    
+
     # disconnect from the database
     message(" - Disconnecting from ", db)
     dbDisconnect(con)
